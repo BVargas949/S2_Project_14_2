f87191805ba6554821c486700ac8f678990b584c
Bryan Vargas <BVargas949@west-mec.org>
2019-04-08 17:42:49 -0700

    Final Commit: Ready for Review
    For loops and if statements were used to organize information before placing them in the cart.

--- sub_cart.js
+++ sub_cart.js
@@ -22,17 +22,40 @@
 
 */
 
+//Once the page loads the setupCart function shall run.
 window.onload = setupCart;
 
+//This function is meant to run the "addItem" function once the Buttons are clicked.
 function setupCart() {
       var addButtons = document.getElementsByClassName("addButton");
 
+      //Loops through all the buttons, listening for clicks
       for (var i = 0; i < addButtons.length; i++) {
             document.addEventListener("onclick", addItem);
-
       }
 }
 
-function addItem() {
+// The "addItem" function is meant to search for values of the items and place them in the foodcart aside.
+function addItem(e) {
 
+      //each variable holds a certain value. They are placed together and a copy is made that shall be added to the aside (Cart);
+      var foodItem = e.nextElementSibling.value;
+      var foodID = foodItem.getAttribute("id");
+      var foodDescription = foodItem.cloneNode(true);
+      var cartBox = document.getElementById("cart");
+      var duplicateOrder = false;
+
+      //This foor loop is meant to compare ID's before changing the value of each child, for every 
+      for (var i = 0; i < cartBox.length; i++) {
+            if (cartBox.childNodes == foodID) {
+                  cartBox.firstChild() += 1;
+            }
+      }
+      //This if statement is meant to create certain parts that go in the cart, such as orderCount
+      if (duplicateOrder !== false) {
+            var orderCount = document.createElement("span");
+            orderCount = "1";
+            foodDescription += orderCount;
+            foodDescription.appendChild(cartBox);
+      }
 }
